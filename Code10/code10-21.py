e970cbed0b47e43cf6fecf44aebc620cb4afb482
shon0921 <43308838+shon0921@users.noreply.github.com>
2024-10-30 09:08:18 +0900

    Add files via upload

---
+++ Code10/code10-21.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter.filedialog import *
+
+window = Tk()
+window.geometry('400x100')
+
+label1 = Label(window, text="선택된 파일 이름")
+label1.pack()
+
+filename = askopenfilename(parent = window, mode = "w", defaultextension= ".jpg", 
+                           filetypes = (("JPG 파일","*.jpg;*.jepg"),("모든 파일","*.*")))
+
+label1.configure(text = saveFp)
+saveFp.close()
+
+window.mainloop()
